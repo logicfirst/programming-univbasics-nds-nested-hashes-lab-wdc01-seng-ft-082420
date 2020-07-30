8d35c2d0e66c0adcebd96063c2fd9a91736b3b58
Scott H Lee <slee@logicfirstllc.com>
2020-07-30 13:28:36 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,7 +20,7 @@ def literal_aoh
     JOELLE_VD = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     PAT_M =  { name:  "Pat Monteseian", occupation: "Staff" },
     KATE_G = { name:  "Kate Gompert", occupation: "None" },
-    
+    BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
 end
 
